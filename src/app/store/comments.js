4336279a95a58058884fa59a740c416686b5e722
nova8 <aaa.auksutat@gmail.com>
2023-05-16 01:53:43 +0200

    redux-fast-company-task3

---
+++ src/app/store/comments.js
@@ -0,0 +1,67 @@
+import { createAction, createSlice, nanoid } from "@reduxjs/toolkit";
+import commentService from "../services/comment.service";
+import { getCurrentUserId } from "./users";
+
+const commentsSlice = createSlice({
+    name: "comments",
+    initialState: {
+        entities: null,
+        isLoading: true,
+        error: null
+    },
+    reducers: {
+        commentsRequested: (state) => {
+            state.isLoading = true;
+        },
+        commentsReceived: (state, action) => {
+            state.entities = action.payload;
+            state.isLoading = false;
+        },
+        commentsRequestFailed: (state, action) => {
+            state.error = action.payload;
+            state.isLoading = false;
+        },
+        commentCreated: (state, action) => {
+            state.entities.push(action.payload);
+        }
+    }
+});
+
+const { reducer: commentsReducer, actions } = commentsSlice;
+const {
+    commentsRequested,
+    commentsReceived,
+    commentsRequestFailed,
+    commentCreated
+} = actions;
+const addCommentRequested = createAction("comments/addCommentRequested");
+
+export const loadCommentsList = (userId) => async (dispatch) => {
+    dispatch(commentsRequested());
+    try {
+        const { content } = await commentService.getComments(userId);
+        dispatch(commentsReceived(content));
+    } catch (error) {
+        dispatch(commentsRequestFailed(error.message));
+    }
+};
+export const createComment = (payload) => async (dispatch, getState) => {
+    dispatch(addCommentRequested(payload));
+    const comment = {
+        ...payload,
+        _id: nanoid(),
+        created_at: Date.now(),
+        userId: getCurrentUserId()(getState())
+    };
+    try {
+        const { content } = await commentService.createComment(comment);
+        dispatch(commentCreated(content));
+    } catch (error) {
+        dispatch(commentsRequestFailed(error.message));
+    }
+};
+export const getComments = () => (state) => state.comments.entities;
+export const getCommentsLoadingStatus = () => (state) =>
+    state.comments.isLoading;
+
+export default commentsReducer;
